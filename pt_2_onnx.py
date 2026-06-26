2abf6e7089cd04bc2c523df80cfa9ee431e5c4ce
UltralyticsAssistant <web@ultralytics.com>
2026-06-26 10:13:13 +0000

    Auto-format by https://ultralytics.com/actions

--- pt_2_onnx.py
+++ pt_2_onnx.py
@@ -1,3 +1,4 @@
 from ultralytics import YOLO
-model = YOLO('D:/Computer_Vision/ultralytics-main/runs/train/exp/weights/best.pt')
-model.export(format = 'onnx')
+
+model = YOLO("D:/Computer_Vision/ultralytics-main/runs/train/exp/weights/best.pt")
+model.export(format="onnx")
