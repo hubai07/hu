c57a00aed538f5c1df2f41153f62cbbb40464ce6
UltralyticsAssistant <web@ultralytics.com>
2026-06-26 10:29:31 +0000

    Auto-format by https://ultralytics.com/actions

--- detect.py
+++ detect.py
@@ -1,10 +1,7 @@
 from ultralytics import YOLO
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     model = YOLO("./runs/train/exp/weights/best.pt")
     model.predict(
-        source="./data/train/images/0b725f9f-frame_0740.jpg",
-        imgsz=640,
-        project='./datect',
-        name='exp',
-        save=True
+        source="./data/train/images/0b725f9f-frame_0740.jpg", imgsz=640, project="./datect", name="exp", save=True
     )
