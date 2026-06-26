891bcc75ad06a13cb7979d31794d96b6ec43225f
UltralyticsAssistant <web@ultralytics.com>
2026-06-26 10:25:38 +0000

    Auto-format by https://ultralytics.com/actions

--- fp32_2_int8.py
+++ fp32_2_int8.py
@@ -1,5 +1,5 @@
+from onnxruntime.quantization import QuantType, quantize_dynamic
 
-from onnxruntime.quantization import quantize_dynamic, QuantType
 # 换成你绝对路径，避免相对路径找不到文件
 model_fp32 = "./best.onnx"
 model_quant = "./model_int8.onnx"
@@ -9,6 +9,6 @@ quantize_dynamic(
     model_input=model_fp32,
     model_output=model_quant,
     weight_type=QuantType.QInt8,
-    op_types_to_quantize=["Conv", "MatMul"]
+    op_types_to_quantize=["Conv", "MatMul"],
 )
 print(f"量化完成，量化模型保存路径：{model_quant}")
